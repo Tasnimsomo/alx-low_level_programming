0042ae3d38f8da66e663b9bcf4d4e23f96be50fb
Tasnim Somo <tasnasomo@gmail.com>
2023-07-19 23:31:55 -0700

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/** main - function that prints alphabets in lowercase
+  *
+ */
+
+void print_alphabet(void)
+{
+        int i;
+        for (i = 'a' ; i <= 'z' ; i++)
+        putchar(i);
+        putchar('/n');
+}
+
+/** main - the main main function
+  *
+  *Return: Always 0.
+ */
+  
+ int main(void)
+{
+    print_alphabet();
+    return (0);
+ }
