029cec616b4367079564931cf1f2ebb2794326df
Tasnim Somo <tasnasomo@gmail.com>
2023-07-20 14:17:32 -0700

    1-isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _isdigit(int c);
-
-int main(void)
-{
-        int c;
-        c = 'l';
-        printf("%c is%s a digit \n", c, _isdigit(c) ? "" : " not");
-        return (0);
-}
-
-
 int _isdigit(int c)
 {
         if(c >= '0' && c <= '9')
