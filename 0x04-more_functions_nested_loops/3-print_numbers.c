d6675741fa2afa1f624b5208ee333c9a51410daa
Tasnim Somo <tasnasomo@gmail.com>
2023-07-20 16:50:00 -0700

    Numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *void print_numbers(void)
+ *
+ * Return: 0 or 1
+ */
+
+int void print_numbers(void)
+{
+        for(i = 0 ; i < = 9 ; i++)
+        {
+            putchar(i);
+        }
+        putchar('\n');
+}
