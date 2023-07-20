92f1d6ce48183f59644e7d93a6ed51b32a3195db
Tasnim Somo <tasnasomo@gmail.com>
2023-07-19 22:57:17 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/** main - prints putchar followed by a new line
+ ** return: 0
+**/
+ 
+void print_putchar()
+{
+        printf("_");
+        printf("p");
+        printf("u");
+        printf("t");
+        printf("c");
+        printf("h");
+        printf("a");
+        printf("r");
+        printf("\n");
+}
+
+int main()
+{
+        print_putchar();
+        return 0;
+}
