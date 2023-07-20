5ce18ededb729585589fd67e33aa66d5e5f2f090
Tasnim Somo <tasnasomo@gmail.com>
2023-07-20 15:49:29 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,16 +7,14 @@
  * Return: 0 or 1
 */
 
-int _isupper(int c);
-
 int _isupper(int c)
  {
         if (c >= 'A' && c <= 'Z')
-        {
+        
             return (1);
-        }
+        
         else
-        {
+        
             return (0);
-        }
+        
  }
