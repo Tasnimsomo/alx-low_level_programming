654be3560ce56f1f537bd93065cd6ba4bb3f16bb
Tasnim Somo <tasnasomo@gmail.com>
2023-07-20 13:48:04 -0700

    _isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+ {
+        if (c >= 'A' && c <= 'Z')
+        {
+            return (1);
+        }
+        else
+        {
+            return (0);
+        }
+ }
