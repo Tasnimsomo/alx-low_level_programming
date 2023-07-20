bab59692efffeb1b3bee61a26e2c6c7cf61bae7f
Tasnim Somo <tasnasomo@gmail.com>
2023-07-20 15:53:21 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
-
- /** main main function
-   *
-   *Return: Always 0.
-  */
-
- void print_alphabet(void);
-
- int main(void)
- {
-        print_alphabet();
-        return (0);
- }
+#include "main.h"  
  
  
  /**
