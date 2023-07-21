f327b7060eb56df4e1120a6b8b2a3adf01f540aa
Tasnim Somo <tasnasomo@gmail.com>
2023-07-21 10:39:45 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,14 +4,8 @@
  * isupper - checks if its uppercase
  *
  * Return: Always 0.
-*/
+ */
 
- int isdigit(int c);
- int main(void)
- {
-    char c = '4';
-    printf("%c is %s a digit",c,isdigit(c)? "" : "not");
- }
  int _isdigit(int c)
  {
      if(c >= '0' && c<= '9')
