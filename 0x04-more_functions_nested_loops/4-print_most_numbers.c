6369dfef722bcb37d441eba33328d7ac7f2e51da
Tasnim Somo <tasnasomo@gmail.com>
2023-07-21 11:34:02 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,12 @@
 
 void print_most_numbers(void)
 {
-            char c ;
-            for(c = '0' ; c <= '9' ; c++)
-    {
-                if (c == '2' || c == '4')
-        {
-                continue;
-        }
-        _putchar(c);
+	char c;
 
-    }
-        _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
 }
