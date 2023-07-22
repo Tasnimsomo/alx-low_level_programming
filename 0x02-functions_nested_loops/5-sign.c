5efb5fbcb980f1f3a19371301f0caf435eba2220
Tasnim Somo <tasnasomo@gmail.com>
2023-07-21 22:35:22 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,15 +14,15 @@ int print_sign(int n)
         return (1);
     }
 
-    else if (n < 0)
+    else if (n == 0)
        {
 
-        _putchar('-');
+        _putchar('0');
         return (0);
         }
     else
         {
-           _ putchar('0');
+           _ putchar('-');
             return (-1);
 }
 
