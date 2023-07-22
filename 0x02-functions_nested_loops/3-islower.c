c33f48b560b9000f39950f92da458935d510d57f
Tasnim Somo <tasnasomo@gmail.com>
2023-07-21 20:56:56 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *int _islower(int c) - checks for lowercase letters.
+ *
+ *Return: 1 if lowercase or 0 if otherwise.
+ */
+
+int _islower(int c)
+{
+            if(c >= 'a' && c<= 'z')
+                return (1);
+            else
+                return (0);
+
+}
+
