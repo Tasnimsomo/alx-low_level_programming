1e6b5ecb6f1ac02514abfdff5be607cdda654be7
Tasnim Somo <tasnasomo@gmail.com>
2023-07-24 13:34:37 -0700

    Half a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,23 +8,18 @@
 
 void puts_half(char *str)
 {
+int len = strlen(str);
 int half_len;
-int i;
-int len = 0;
-while (str[len] != '\0') {
-len++;
-}
-half_len = len / 2;
-if (len % 2 != 0)
+if (len % 2 == 0)
 {
-half_len = (len - 1) / 2;
+half_len = len / 2;
 }
 else
 {
 half_len = (len - 1) / 2;
-}   
-for (i = half_len; i < len; i++)
-{
+}
+for (int i = half_len; i < len; i++) {
 _putchar(str[i]);
 }
+_putchar('\n');
 }
