7d54b0f909ef0329b0dbf44be724862a73270a5c
Tasnim Somo <tasnasomo@gmail.com>
2023-07-24 08:40:29 -0700

    Do not swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @ a : character to check.
- * @ b : character to check.
+ * @a : character to check.
+ * @b : character to check.
  * swaps the values of two integers
  */
 void swap_int(int *a, int *b)
