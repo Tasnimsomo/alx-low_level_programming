1af9629e12e91a33b9f4f0383f73ad94f9a2ffdf
Tasnim Somo <tasnasomo@gmail.com>
2023-07-24 10:05:32 -0700

    Half the lies they tell about me are not true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character
+ * _putchar - print each character
+ * @str: char to check
+ *
+ *Return: 0 is success
+ */
+
+void puts2(char *str)
+{
+    while (*str != 0)
+    {
+        putchar('*str');
+    }
+        putchar('\n');
+}
