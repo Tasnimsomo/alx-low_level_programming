bbc4d3876f4ced5a7ee9b533c941840a1354affd
Tasnim Somo <tasnasomo@gmail.com>
2023-07-24 12:57:05 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - printing a string in reverse
+ * @s: the string to be printed in rev
+ */
+
+void reverse_string(char *str)
+{
+int i, j;
+char temp;
+int len = 0;
+while (str[len] != '\0') {
+len++;
+}
+for (i = 0, j = len - 1; i < j; i++, j--) {
+temp = str[i];
+str[i] = str[j];
+str[j] = temp;
+}
+}
