16b359f21f06e14ba38202bbe071290dc809aa9f
Tasnim Somo <tasnasomo@gmail.com>
2023-07-24 12:12:21 -0700

    Every other string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,7 @@
  */
 void puts2(char *str)
 {
-int = i;
+int i;
 for (i = 0 ; str[i] != '\0' ; i++)
 {
 if (i % 2 == 0)
@@ -17,5 +17,5 @@ if (i % 2 == 0)
 _putchar(str[i]);
 }
 }
-_putchar('\n')
+_putchar('\n');
 }
