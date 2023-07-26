21b33a10a29956372cec1548cec784d3915207b9
Tasnim Somo <tasnasomo@gmail.com>
2023-07-25 23:35:50 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcat - concatenates two strings
+ *@dest -to test.
+ *@src - to test.
+ *
+ *Returns: Pointer to resulting string dest.
+ */
+char *_strcat(char *dest, char *scr)
+{
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+{
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i+j] = src[j];
+}
+dest[i+j] = '\0';
+}
+return dest;
+}
