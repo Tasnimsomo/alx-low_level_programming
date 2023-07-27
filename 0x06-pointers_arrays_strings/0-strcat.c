ea62ace12cc38631de981d24a4e27bfdc2607daa
Tasnim Somo <tasnasomo@gmail.com>
2023-07-27 13:36:43 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-while (dest[i] != '\0')
+int i ,j; 
+for (i = 0; dest[i] != '\0' ; i++ ) 
 {
-i++;
+for(j = 0; src[j] != '\0'; j++)
+{
+dest[i+j] = src[j];
 }
-int j = 0;
-while (src[j] != '\0')
-{
-dest[i + j] = src[j];
-j++;
-}        
+}
 dest[i + j] = '\0';
 return dest;
 }
