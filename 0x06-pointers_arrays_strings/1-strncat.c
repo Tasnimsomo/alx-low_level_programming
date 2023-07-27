3cf796c07148d26aabbc853c141f88c232d1f2f5
Tasnim Somo <tasnasomo@gmail.com>
2023-07-27 15:21:15 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,16 +9,18 @@
  */
 
 char *_strncat(char *dest, char *src, int n) {
-while (*dest) {
-dest++;
+int count = 0, count2 = 0;
+while (*(dest + count) != '\0')
+{
+count++;
 }
-int i = 0;
-while (*src && i < n) {
-*dest = *src;
-dest++;
-src++;
-i++;
+while (count2 < n)
+{
+*(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
+break;
+count++;
+count2++;
 }
-*dest = '\0';
-return dest;
+return (dest);
 }
