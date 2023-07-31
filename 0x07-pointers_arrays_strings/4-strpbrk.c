369fe4eb46636411c9dfede411c5e8634288b8fe
Tasnim Somo <tasnasomo@gmail.com>
2023-07-31 11:47:40 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,15 +11,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-int i;
-while (*s)
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
 {
-for (i = 0; accept[i]; i++)
+for (j = 0; accept[j] != '\0'; j++)
 {
-if (*s == accept[i])
-return (s);
+if (s[i] == accept[j])
+{
+return (s + i);
 }
-s++;
 }
-return (NULL);
+}
+return (0);
 }
