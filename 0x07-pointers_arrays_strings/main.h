610bc0761d322728f4bff735084bf9594fb3497d
Tasnim Somo <tasnasomo@gmail.com>
2023-07-31 12:40:33 -0700

    chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,1 @@
-
-void print_diagsums(int *a, int size);
-void print_chessboard(char (*a)[8]);
-char *_strstr(char *haystack, char *needle);
-char *_strpbrk(char *s, char *accept);
-unsigned int _strspn(char *s, char *accept);
-char *_strchr(char *s, char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_memset(char *s, char b, unsigned int n); 
-int putchar(); 
-int putchar(int); 
-int _putchar(char c); 
-int _putchar(int c); 
+int _putchar(); 
