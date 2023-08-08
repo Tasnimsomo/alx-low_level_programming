57d9ccee7338d224cd8d9f5f4752d632021e2513
Tasnim Somo <tasnasomo@gmail.com>
2023-08-08 13:46:11 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *char *create_array
+ *@c - charcter to test
+ *
+ *Returns pointer to array or null if it fails
+ */
+char *create_array(unsigned int size, char c)
+{ 
+if(size == 0)
+{
+return(NULL);
+}
+char *array = (char *)malloc(size * sizeof(char));
+if (array == NULL){
+return (NULL);
+}
+for (unsigned int i = 0; i < size; i++)
+{
+array[i] = c;
+}return (array;)
+}
