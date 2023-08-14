98126f7cfa4e958b7b36cdf3d3a7ee2834e0d763
Tasnim Somo <tasnasomo@gmail.com>
2023-08-13 17:16:00 -0700

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,14 @@
 
 /**
  *_puts_recursion - prints a string followed by a new line.
- * @s - character to test.
+ * @ *s - character to test.
  *
  *Return : string followed by new line.
  */
 void _puts_recursion(char *s)
 {
-if(*s == '\0')
+if (*s == '\0')
 {
-_putchar(s);
 _putchar('\n');
 }
 else
