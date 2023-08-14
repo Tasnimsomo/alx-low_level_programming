a65acca72a277d9610271184a83c2d65e6919567
Tasnim Somo <tasnasomo@gmail.com>
2023-08-13 17:19:52 -0700

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  *_puts_recursion - prints a string followed by a new line.
- * @s - string to be printed.
+ * char *s - string to be printed.
  *
  *Return : string followed by new line.
  */
