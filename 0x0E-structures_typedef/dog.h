53fc8e559290eaf5c26b5075bea4bd059c8ef829
Tasnim Somo <tasnasomo@gmail.com>
2023-08-14 16:24:02 -0700

    innit_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,4 @@
-struct dog {
-    char *name;
-    float age;
-    char *owner;
-};
-
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d); 
