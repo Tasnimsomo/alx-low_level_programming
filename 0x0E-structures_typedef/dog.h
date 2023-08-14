432600bbb72a1fe38a137cc027160b7cfd8386e1
Tasnim Somo <tasnasomo@gmail.com>
2023-08-14 15:28:34 -0700

    innit_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,6 @@
-#include <stdio.h>
-
-/**
- *struct dog - struct to be defined.
- *@name: name of dog.
- *@age: age of dog.
- *@owner: owner of dog.
- */
-struct dog
-{
-char *name;
-float age;
-char *owner;
-}
-;
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
 
