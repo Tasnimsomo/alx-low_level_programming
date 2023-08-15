b63f500bd06e9700a28928c5bf152efcb95b04fe
Tasnim Somo <tasnasomo@gmail.com>
2023-08-14 17:59:08 -0700

    print_dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+int _putchar(char c);
+/**
+*struct dog - Initialize struct dog
+*@name:name type char
+*@age: age type float
+*@owner: name of owner tyme char
+*
+*Description: name, age and owner of a dog
+*/
+
 typedef struct dog
 {
 char *name;
@@ -9,4 +22,4 @@ void print_dog(struct dog *d);
 typedef dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
+#endif /* DOG_H */
