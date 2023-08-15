be29fa50197e2aafbea0e3a397ef92d5cacde217
Tasnim Somo <tasnasomo@gmail.com>
2023-08-14 17:26:02 -0700

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,25 +1,37 @@
 #include "dog.h"
-#include "stdio.h"
+#include <stdio.h>
 
 /**
  *print_dog-prints struct dog.
  *@d : structure.
  */
-
 void print_dog(struct dog *d)
 {
 if (d == NULL)
 {
 return;
 }
-else if(((*d).name == NULL || (*d).owner ==  NULL))
+if((*d).age == 0)
+{
+printf("nil");
+}
+else
+{
+printf("Age: %f\n", (*d).age);
+}
+if((*d).name == NULL)
 {
 printf("nil");
 }
 else
 {
 printf("Name: %s\n", (*d).name);
-printf("Age: %f\n", (*d).age);
-printf("Owner: %s\n", (*d).owner);
 }
+if((*d).owner == NULL)
+{
+printf("nil");
+}
+else
+{
+printf("Owner: %s\n", (*d).owner);
 }
