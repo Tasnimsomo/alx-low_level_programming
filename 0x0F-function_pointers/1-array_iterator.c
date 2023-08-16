da8b75eb521e44e0e99ce276eb34b0fac3163ec9
Tasnim Somo <tasnasomo@gmail.com>
2023-08-16 15:56:43 -0700

    array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - prints an integer
+ * @action: function pointer
+ *@array : argument
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (action != NULL)
+for (i = 0; i < size ; i++)
+{
+action(array[i]);
+}
