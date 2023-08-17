ddca6032a14396ef6c9c5a47e25451979af18d3d
Tasnim Somo <tasnasomo@gmail.com>
2023-08-16 17:00:42 -0700

    int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+/**
+ * int_index - checks for an integer
+ * @array: array
+ * @size : size of arra
+ * @cmp : function pointer
+ * Return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i, count;
+count = 0;
+if (array != NULL && cmp != NULL)
+{
+for (i = 0; i < size; i++)
+{
+if(cmp(array[i]) != 0)
+{
+count++;
+return (i);
+}
+}
+}
+if(size <= 0 || count == 0)
+{
+return (-1);
+}
+else
+{
+return (0);
+}   
+}
