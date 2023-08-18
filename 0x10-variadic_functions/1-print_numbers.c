d6efa2f2a0501b23deb82a7238f2a9703700a32f
Tasnim Somo <tasnasomo@gmail.com>
2023-08-17 18:49:38 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include "variadic_functions.h"
+
 /**
  *print_numbers - prints numbers followed by new line.
  *@separator:string to be printed between numbers
