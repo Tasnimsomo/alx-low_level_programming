6617b8e8a87c6fe8e5a807381aeb7e85b60e6836
Tasnim Somo <tasnasomo@gmail.com>
2023-08-17 18:36:11 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,17 +12,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list ptr;
 va_start(ptr, n);
 unsigned int i;
+for (i = 0; i < n; i++)
+{
 if (separator == NULL)
 {
 printf("%d", va_arg(ptr, unsigned int));
 }
 else
 {
-for (i = 0; i < n; i++)
-{
 printf("%d%s",va_arg(ptr,unsigned int),separator);
+}
 }
 printf("\n");
 va_end(ptr);
-}
 }
