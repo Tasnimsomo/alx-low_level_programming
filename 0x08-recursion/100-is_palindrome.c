1aa6070d138e5efce1e8aa26aee118f7944649af
Tasnim Somo <tasnasomo@gmail.com>
2023-08-19 17:35:35 -0700

    is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,8 +3,10 @@
 #include "main.h"
 
 /**
- *is_palindrome- function that returns 1 if a string is a palindrome and 0 if not.
+ *is_palindrome- returns 1 if a string is a palindrome and 0 if not.
  *@s: character to test.
+ *@left: character to test.
+ *@right: character to test.
  *Return:1 if a string is a palindrome and 0 if not.
  */
 int is_palindrome_recursive(char *s, int left, int right)
@@ -13,13 +15,15 @@ if (left >= right)
 {
 return (1);  
 }
-if (s[left] != s[right]) {
+if (s[left] != s[right])
+{
 return (0); 
 }
-return is_palindrome_recursive(s, left + 1, right - 1);
+return (is_palindrome_recursive(s, left + 1, right - 1));
 }
-int is_palindrome(char *s) {
+int is_palindrome(char *s)
+{
 int length = strlen(s);
-return is_palindrome_recursive(s, 0, length - 1);
+return (is_palindrome_recursive(s, 0, length - 1));
 }
 
