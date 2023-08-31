81fe64d2729d0027c5bcfa49b9e6b1b31e0c95eb
Tasnim <tasnasomo@gmail.com>
2023-08-31 07:51:27 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,16 +3,16 @@
 /**
  *int _strlen_recursion- returns length of string.
  *@s: string to test.
- *Return: Nothing.
+ *Return: Length of string.
  */
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-return;
+return (0);
 }
 else
 {
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
 }
