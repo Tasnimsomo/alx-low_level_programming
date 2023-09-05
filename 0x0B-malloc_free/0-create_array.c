c7dc09ebf3182127e3b60d9c66139d49321d732f
Tasnim <tasnasomo@gmail.com>
2023-09-05 15:28:12 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,12 +9,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
+char *array;
 unsigned int i;
 if (size == 0)
 {
 return NULL;
 }
-char *array = (char *)malloc(size *sizeof(char));
+array = (char *)malloc(size *sizeof(char));
 if (array == NULL)
 {
 return NULL;
