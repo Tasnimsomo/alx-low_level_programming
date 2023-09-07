7fa64a62598ac6161831536dfe53319499ca3610
Tasnim <tasnasomo@gmail.com>
2023-09-07 15:35:27 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,13 +7,15 @@
  * Return: a pointer to new array.
  */
 int *array_range(int min, int max)
+{
+int *arr, i, size;
 if (min > max)
 return (NULL);
-int *arr = malloc((max - min + 1) * sizeof(int));
+size = max - min + 1;
+arr = malloc(sizeof(int) * size);
 if (arr == NULL)
 return (NULL);
-for (int i = 0; i <= max - min; i++) {
-arr[i] = min + i;
-}
+for (i = 0; i < size; i++)
+arr[i] = min++;
 return (arr);
 }  
