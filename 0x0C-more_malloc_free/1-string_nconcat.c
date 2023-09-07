4b42d2acde7c9aac31f18fa96bd44c95f2e9b0c3
Tasnim <tasnasomo@gmail.com>
2023-09-07 14:37:34 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,30 +9,28 @@
  * Return: entire string.
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n) {
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-
-    unsigned int s1_len = strlen(s1);
-    unsigned int s2_len = strlen(s2);
-    if (n >= s2_len)
-        n = s2_len;
-
-    unsigned int concat_len = s1_len + n;
-    char *concat = (char *)malloc((concat_len + 1) * sizeof(char));
-    if (concat == NULL)
-        return NULL;
-
-    unsigned int i, j;
-    for (i = 0; i < s1_len; i++) {
-        concat[i] = s1[i];
-    }
-    for (j = 0; j < n; j++) {
-        concat[i + j] = s2[j];
-    }
-    concat[i + j] = '\0';
-
-    return concat;
-}
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int len = 0;
+unsigned int i;
+unsigned int j;
+char *concat;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (i = 0; s1[i] != '\0'; i++)
+len1++;
+for (i = 0; s2[i] != '\0' && i < n; i++)
+len2++; 
+concat = malloc(sizeof(char) * (len + 1));
+if (concat == NULL)
+return (NULL);
+len = 0;
+for (i = 0; s1[i] != '\0'; i++)
+concat[len++] = s1[i];
+for (j = 0; s2[j] != '\0' && j < n; j++)
+concat[len++] = s2[j];
+concat[len] = '\0';
+return (concat);
+} 
