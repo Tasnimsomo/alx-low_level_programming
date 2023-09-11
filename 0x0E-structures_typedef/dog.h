8225aca319ccfe78e042ff04d42acde3d272ef82
Tasnim <tasnasomo@gmail.com>
2023-09-11 09:58:10 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct that stores dog info.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ */
+struct dog
+{
+    char* name;
+    float age;
+    char* owner;
+};
+
+#endif /* DOG_H */
