04658b68ce103f2dd802fcc9b814c19d2d4c6baf
Tasnim <tasnasomo@gmail.com>
2023-09-11 12:11:18 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,18 +10,18 @@
  */
 void print_dog(struct dog *d)
 {
-if (d == NULL)
+if(((*d).name ==  NULL))
 {
-return;
+printf("Name: (nil)\n");
 }
-else if(((*d).name == NULL || (*d).owner ==  NULL))
+else if ((*d).owner ==  NULL))
 {
-printf("nil\n");
+printf("Age: (nil)\n");  
 }
 else
 {
 printf("Name: %s\n", (*d).name);
 printf("Age: %f\n", (*d).age);
-printf("Owner: %s", (*d).owner);
+printf("Owner: %s\n", (*d).owner);
 }
 }
