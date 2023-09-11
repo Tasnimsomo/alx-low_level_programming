2316786188b51491666089e37712381cac7859b6
Tasnim <tasnasomo@gmail.com>
2023-09-11 11:25:21 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * print_dog - Prints all the data of a dog
+ * @d: A dog structure
+ *
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+else if(((*d).name == NULL || (*d).owner ==  NULL))
+{
+printf("nil");
+}
+else
+{
+printf("Name: %s\n", (*d).name);
+printf("Age: %f\n", (*d).age);
+printf("Owner: %s\n", (*d).owner);
+}
+}
