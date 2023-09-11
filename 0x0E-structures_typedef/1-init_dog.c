cdaba37134ae571fecc6aed6a010db695ff740e0
Tasnim <tasnasomo@gmail.com>
2023-09-11 10:32:01 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,6 +8,12 @@
  * @owner: owner of dog.
  */
 
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 (*d).name = name;
