56cf42c198c45cf71df52f7932f348a8022ce994
Tasnim <tasnasomo@gmail.com>
2023-09-11 10:28:54 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,10 @@ float age;
 char *owner;
 };
 
-#endif /* DOG_H */
+
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
