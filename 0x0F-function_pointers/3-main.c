bf47065c150b9e68460dce1f99d0cb369ccecf29
Tasnim <tasnasomo@gmail.com>
2023-09-13 15:13:14 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,12 +20,13 @@ exit (98);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
-if (argv[2][1] != '\0')
+op_func = get_op_func(argv[2]);
+if (op_func == NULL)
 {
 printf("Error\n");
-exit(99);
+exit (99);
 }
-result = get_op_func(argv[2])(num1, num2);
+result = op_func(num1, num2);
 printf("%d\n", result);
-return 0;
+return (0);
 }
