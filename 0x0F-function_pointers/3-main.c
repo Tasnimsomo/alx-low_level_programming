be45b1232b940b6712effc2b4d6335d527cb519f
Tasnim <tasnasomo@gmail.com>
2023-09-16 10:05:02 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,14 +5,14 @@
 int main(int argc, char *argv[])
 {
 int (*op_func)(int, int);
+int num1;
+int num2;
+int result;
 if (argc != 4)
 {
 printf("Error\n");
 exit (98);
 }
-int num1;
-int num2;
-int result;
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 if (argv[2][1] != '\0')
