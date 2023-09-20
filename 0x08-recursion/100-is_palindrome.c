38524e69b460b6ea6b08e797cbc4b98b3eb1dfb2
Tasnim <tasnasomo@gmail.com>
2023-09-19 22:44:01 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * length - calculates length of string
+ * @s: input
+ * Return: 0
+ */
+
+int length(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + long_1(s + 1));
+	}
+	return (0);
+
+}
+/**
+ * compare -compares strings at beginning and end
+ * @s: input
+ * @l: input
+ * Return: 0
+ */
+int compare(char *s, int l)
+{
+if (l <= 0)
+{
+return (1);
+}
+if (*s == *(s + (l - 1)))
+{
+return (compare(s + 1, l - 2));
+}
+else
+{
+return (0);
+}
+}
+
+/**
+ * is_palindrome - checks if palindrome
+ * @s: input
+ * Return: 0
+ */
+int is_palindrome(char *s)
+{
+int l;
+l = length(s);
+return (compare(s, l));
+}
