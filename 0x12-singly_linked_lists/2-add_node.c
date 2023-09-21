333935b2365af939f9d32c961b023ec5581b9a56
Tasnim <tasnasomo@gmail.com>
2023-09-21 14:27:58 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,6 +13,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *newNode = (list_t*)malloc(sizeof(list_t));
+size_t len = 0;
 if (str == NULL)
 {
 return (NULL);
@@ -26,6 +27,10 @@ if (newNode->str == NULL)
 {
 free(newNode);
 return (NULL);
+}
+for (len = 0; str[len];)
+{
+len++;
 }
 newNode->next = *head;
 *head = newNode;
