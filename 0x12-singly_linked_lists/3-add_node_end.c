7fe55f3ec37b055e539bad3e7b1abd777db70797
Tasnim <tasnasomo@gmail.com>
2023-09-21 21:54:28 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,7 +13,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 size_t len = 0;
-list_t *newNode = (list_t*)malloc(sizeof(list_t));
+list_t *newNode = (list_t *)malloc(sizeof(list_t));
 if (newNode == NULL)
 {
 return (NULL);
