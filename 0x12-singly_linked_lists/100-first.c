f48bdbb7d2ed3e852d02082762014dbfbe998926
Tasnim <tasnasomo@gmail.com>
2023-09-25 23:00:34 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,11 @@
  *
  * Return: no return.
  */
+void __attribute__((constructor)) before_main(void) {
+/* You're beat! and yet, you must allow,
+I bore my house upon my back!
+*/
+}
 void before_main(void)
 {
 printf("You're beat! and yet, you must allow,\n");
