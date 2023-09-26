fb4a00488eeb6799c142e6f0f5651c81a4766f04
Tasnim <tasnasomo@gmail.com>
2023-09-26 10:03:46 -0700

    print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *print_listint - prints all the elements of a list_t list.
+ *print_listint - prints all the elements of the list.
  *@h: head of list
  *Return: the number of nodes
  */
