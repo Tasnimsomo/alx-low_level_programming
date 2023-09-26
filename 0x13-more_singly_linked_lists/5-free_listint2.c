64a6b964020b43aa96e6ba4457df00033af61fb9
Tasnim <tasnasomo@gmail.com>
2023-09-26 10:46:26 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,18 +10,12 @@
 void free_listint2(listint_t **head)
 {
 listint_t *current;
-listint_t **temp = head;
 if (head == NULL)
-{
 return;
-}
 while (*head != NULL)
 {
-current = *head;
-free(current);
+current =  *head;
 *head = (*head)->next;
-}
-
-*head = NULL;
+free(current);
 }
 }
