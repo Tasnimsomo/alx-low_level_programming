bd22883a56d8cf26db06318f312e231a81d1437b
Tasnim <tasnasomo@gmail.com>
2023-09-26 10:20:06 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,7 +10,8 @@
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *newNode = (listint_t *)malloc(sizeof(list_t));
+listint_t *newNode;
+newNode = (listint_t *)malloc(sizeof(list_t));
 if (newNode == NULL)
 {
 return (NULL);
