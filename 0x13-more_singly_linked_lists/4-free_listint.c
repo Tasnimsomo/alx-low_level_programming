f8dc22b83592fa34c250aac5951e35ad513372e4
Tasnim <tasnasomo@gmail.com>
2023-09-26 10:34:54 -0700

    free.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *free_list - Frees a list
+ *@head: A pointer to the list head
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+listint_t *tmp;
+while (head)
+{
+tmp = head->next;
+free(head);
+head = tmp;
+}
+}
