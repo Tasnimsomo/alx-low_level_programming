ce28b47caa9b025bc4d6bb2f866f69b8ae443500
Tasnim <tasnasomo@gmail.com>
2023-09-26 04:45:30 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(int num_bytes) {
-unsigned char *ptr = (unsigned char *)print_opcodes;
-int i;
-for (i = 0; i < num_bytes; i++)
-{
-printf("%02x ", ptr[i]);
-}
-printf("\n");
-}
+/**
+*main -  program that prints the opcodes of its own main function.
+*@argc: number of arguments passed to the program
+*@argv: array of arguments
+*
+*Return: on success, 1 or 2 in case of failure
+*/
+
 int main(int argc, char *argv[])
 {
+
+int index, bytes;
+int (*address)(int, char **) = main;
+unsigned char opcode;
+
 if (argc != 2)
 {
 printf("Error\n");
-return (1);
+exit(1);
 }
-int num_bytes = atoi(argv[1]);
-if (num_bytes < 0)
+
+bytes = atoi(argv[1]);
+
+if (bytes < 0)
 {
 printf("Error\n");
-return (2);
+exit(2);
 }
-print_opcodes(num_bytes);
+
+for (index = 0; index <  bytes; index++)
+{
+opcode = *(unsigned char *)address;
+printf("%.2x", opcode);
+
+if (index == bytes - 1)
+continue;
+printf(" ");
+address++;
+}
+printf("\n");
 return (0);
 }
