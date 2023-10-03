db0dad7826b3db6d2f6213f25cb32d07af2e4d62
Tasnim <tasnasomo@gmail.com>
2023-10-02 19:41:47 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,4 @@
+ssize_t read_textfile(const char *filename, size_t letters);
+where letters is the number of letters it should read and print
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content); 
