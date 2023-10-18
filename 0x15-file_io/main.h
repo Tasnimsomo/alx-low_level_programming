56b870bdd85b57d46a82f9eda7fc19255bfb208e
Tasnim <tasnasomo@gmail.com>
2023-10-17 22:47:02 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,10 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 where letters is the number of letters it should read and print
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content); 
-#include <stddef.h>
+
