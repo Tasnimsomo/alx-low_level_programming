9b78d298f4b298075210626e33632ee17268d98e
Tasnim <tasnasomo@gmail.com>
2023-10-17 22:45:12 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,4 @@
 #include "main.h"
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stddef.h>
 
 /**
  * read_textfile - reads a text file and prints it to the standard output
@@ -14,34 +9,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t bytesWritten;
-char *buffer  (char *)malloc(letters  1);
+ssize_t file, r, w;
+char *text;
+text = malloc(letters);
+if (text == NULL)
+return (0);
 if (filename == NULL)
+return (0);
+file = open(filename, O_RDONLY);
+if (file == -1)
 {
+free(text);
 return (0);
 }
-int fileDescriptor = open(filename, O_RDONLY);
-if (fileDescriptor == -1)
-{
-return (0);
-}
-if (buffer == NULL)
-{
-close(fileDescriptor);
-return (0);
-}
-ssize_t bytesRead = read(fileDescriptor, buffer, letters);
-if (bytesRead == -1)
-{
-free(buffer);
-close(fileDescriptor);
-return (0);
-}
-bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-free(buffer);
-close(fileDescriptor);
-if (bytesWritten != bytesRead) {
-return (0);
-}
-return (bytesWritten);
+r = read(file, text, letters);
+w = write(STDOUT_FILENO, text, let);
+close(file);
+return (w);
 }
