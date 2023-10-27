ebfc9a32ec88f447a311f2e1974bdcc55d4ce725
Tasnimsomo <tasnasomo@gmail.com>
2023-10-27 07:29:42 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,7 +8,7 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned long int result= n ^ m;
+unsigned long int result = n ^ m;
 unsigned int count = 0;
 while (result > 0)
 {
