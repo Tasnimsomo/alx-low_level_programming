7bc6d6023a381f16afd63e2b7ba816852482ac70
Tasnimsomo <tasnasomo@gmail.com>
2023-10-30 09:49:42 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: name of file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int i;
+int file;
+if (filename == NULL)
+{
+return (-1);
+}
+if (text_content == NULL)
+{
+text_content = "";
+}
+file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (file == -1)
+return (-1);
+i = 0;
+while (text_content[i] != '\0')
+{
+i++;
+}
+write(file, text_content, i);
+return (1);
+}
+
+
+
+
+
